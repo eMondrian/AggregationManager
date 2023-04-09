4fe336ab9f16567fa147659c131f140c83fd29ad
ssemenkoff <ssemenkoff@gmail.com>
2023-04-09 13:56:40 +0400

    Initial commit
    Signed-off-by: ssemenkoff <ssemenkoff@gmail.com>

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createVuesticEssential, VaButton } from 'vuestic-ui'
+import 'vuestic-ui/styles/essential.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+
+app.use(createVuesticEssential({ components: { VaButton } }));
+app.mount('#app')
