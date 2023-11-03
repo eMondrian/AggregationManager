7caa1cfbed6133b0e8359a02ac5343e0d5053bdc
poksleek <ulitsky.vadim@gmail.com>
2023-11-03 21:55:47 +0400

    [feat] remove mocks

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 export { PATH } from './path'
 // NOTE: change to '@/mocks/api/..' for switching to a mocked api
-export { getAggregatesTableData, addAgregation, removeAgregation, getAggregation, updateAggregation, getRunStatus, setRunStatus, resetCommand } from '@/mocks/api/aggregates'
-export { getEventsTableData } from '@/mocks/api/events'
-export { getProcesses } from '@/mocks/api/processes'
-export { getColumnsList, getQuery, getTableList, getQueryPerformance } from '@/mocks/api/wizzard'
-export { getSettings, getTemplates, updateSettings } from '@/mocks/api/settings'
+export { getAggregatesTableData, addAgregation, removeAgregation, getAggregation, updateAggregation, getRunStatus, setRunStatus, resetCommand } from '@/api/aggregates'
+export { getEventsTableData } from '@/api/events'
+export { getProcesses } from '@/api/processes'
+export { getColumnsList, getQuery, getTableList, getQueryPerformance } from '@/api/wizzard'
+export { getSettings, getTemplates, updateSettings } from '@/api/settings'
